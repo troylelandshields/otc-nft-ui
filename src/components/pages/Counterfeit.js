d11d0c6695a4e38b26510be36fe35fc16f7e336e
Troy Shields <troy.leland.shields@gmail.com>
2021-03-27 21:08:50 -0600

    got editable NFT metadata form working

--- src/components/pages/Counterfeit.js
+++ src/components/pages/Counterfeit.js
@@ -5,6 +5,7 @@ import axios from 'axios';
 import config from '../../services/config.js';
 // import Web3 from 'web3';
 import { ethers } from "ethers";
+import moment from 'moment';
 
 // const web3 = new Web3(Web3.givenProvider || "http://localhost:8545");
 // web3.eth.getAccounts().then(console.log);
@@ -23,11 +24,24 @@ function Counterfeit(props) {
 
 	let [ counterfeitContractAddr, setCounterfeitContractAddr ] = useState("");
 	let [ counterfeitTokenId, setCounterfeitTokenId ] = useState("");
+	let [ includeRugPull, setIncludeRugPull ] = useState(false);
+	let [ rugPullDate, setRugPullDate ] = useState(moment().add(1, "month").format("YYYY-MM-DD"));
+	let [ rugPullTime, setRugPullTime ] = useState("12:00");
+	let [ checkoutReady, setIsCheckoutReady ] = useState(false);
 	let [ isCheckingOut, setIsCheckingOut ] = useState(false);
 	let [ walletConnected, setIsWalletConnected ] = useState(false);
 	let [ order, setOrder ] = useState(null);
 	let [ provider, setProvider ] = useState(null);
+	let [ rugPullMeta, setRugPullMeta ] = useState(null);
 
+
+	useEffect(() => {
+		let hasTokenDetails = !!counterfeitContractAddr && !!counterfeitTokenId;
+		if (includeRugPull) {
+			setIsCheckoutReady(hasTokenDetails && !!rugPullDate && !!rugPullTime);
+		}
+		setIsCheckoutReady(hasTokenDetails);
+	}, [counterfeitContractAddr, counterfeitTokenId, includeRugPull, rugPullDate, rugPullTime]);
 
 	useEffect(() => {
 		setIsCheckingOut(!!order);
@@ -49,20 +63,43 @@ function Counterfeit(props) {
 		f();
 	}, [provider]);
 
+	let parseAndSetCounterfeitAddr = (val) => {
+		let parts = val.split("assets");
+		if (!parts || parts.length !== 2) {
+			setCounterfeitContractAddr(val);
+			return
+		}
+		
+		let details = parts[1].split("/");
+		if (!details || details.length !== 3) {
+			setCounterfeitContractAddr(val);
+			return
+		}
+
+		setCounterfeitContractAddr(details[1]);
+		setCounterfeitTokenId(details[2]);
+	}
+
 	let previewCounterfeit = async (e) => {
 		e.preventDefault();
 		console.log(counterfeitTokenId, counterfeitContractAddr);
 
 		try {
+			let rugPullDateTime;
+			if (includeRugPull) {
+				rugPullDateTime = moment(rugPullDate + " " + rugPullTime).format();
+			}
+
 			let resp = await axios.post(`${config.apiHost}/api/v1/preview`, {
 				"Type": "contract",
 				"ContractAddr": counterfeitContractAddr,
-				"CounterfeitTokenID": counterfeitTokenId
+				"CounterfeitTokenID": counterfeitTokenId,
+				"RugPullTime": rugPullDateTime,
 			});
 
 			let o = resp.data;
 
-			const etherValue =ethers.utils.formatEther(o.TotalPriceInWei+"");
+			const etherValue = ethers.utils.formatEther(o.TotalPriceInWei+"");
 			console.log(etherValue);
 
 			o.etherValue = etherValue;
@@ -78,8 +115,10 @@ function Counterfeit(props) {
 	let handleClear = (e) => {
 		e.preventDefault();
 		setCounterfeitContractAddr("");
-		setCounterfeitTokenId("")
+		setCounterfeitTokenId("");
+		setIncludeRugPull(false);
 		setOrder(null);
+		setRugPullMeta(null);
 	};
 
 	let handleConnectWallet = async (e) => {
@@ -102,12 +141,15 @@ function Counterfeit(props) {
 			await axios.post(`${config.apiHost}/api/v1/mint/${order.OrderID}`, {
 				"OrderID": order.OrderID,
 				"PaymentTransactionID": trxn.hash,
-				"DestinationAddress": trxn.from
+				"DestinationAddress": trxn.from,
+				"NFTMeta": rugPullMeta
 			});
 
 			alert("Success! You'll have a newly minted NFT shortly");
 			setCounterfeitContractAddr("");
 			setCounterfeitTokenId("");
+			setIncludeRugPull(false);
+			setRugPullMeta(null);
 			setOrder(null);
 		} catch (e) {
 			console.log("Error finishing transaction", e);
@@ -120,20 +162,48 @@ function Counterfeit(props) {
 			<Form>
 				<Form.Group controlId="counterfeitContractAddr">
 					<Form.Label>Contract Address</Form.Label>
-					<Form.Control disabled={isCheckingOut} value={counterfeitContractAddr} onChange={(e) => setCounterfeitContractAddr(e.target.value)} type="text" placeholder="Copy an ERC721 contract's ethereum address" />
+					<Form.Control disabled={isCheckingOut} value={counterfeitContractAddr} onChange={(e) => parseAndSetCounterfeitAddr(e.target.value)} type="text" placeholder="Paste a link from OpenSea or any ERC721 contract's ethereum address" />
 					<Form.Text className="text-muted">
 						If this is wrong I don't want to be right.
 					</Form.Text>
 				</Form.Group>
 
-				<Form.Group controlId="counterfeitTokenId">
-					<Form.Label>Token ID</Form.Label>
-					<Form.Control disabled={isCheckingOut} value={counterfeitTokenId} onChange={(e) => setCounterfeitTokenId(e.target.value)} type="text" placeholder="Input the Token ID you want" />
-				</Form.Group>
+				{ counterfeitContractAddr ? <>
+					<Form.Group controlId="counterfeitTokenId">
+						<Form.Label>Token ID</Form.Label>
+						<Form.Control disabled={isCheckingOut} value={counterfeitTokenId} onChange={(e) => setCounterfeitTokenId(e.target.value)} type="text" placeholder="Input the Token ID you want" />
+					</Form.Group> 
+
+					<Row>
+						<Col className="col-md-4">
+							<Form.Group controlId="includeRugPull">
+								<Form.Check disabled={isCheckingOut} checked={includeRugPull} onChange={(e) => setIncludeRugPull(e.target.checked)} type="checkbox" label="Include Rug Pull" />
+								<Form.Text className="text-muted">
+									A rugpull will change the metadata of the contract on a certain date; let's you do some pretty classic pranks on your friends. Be aware that this is associated with an increased cost.
+								</Form.Text>
+							</Form.Group>
+						</Col>
+						{ includeRugPull ? <>
+								<Col className="col-md-4">
+									<Form.Group controlId="rugPullTime">
+										<Form.Control disabled={isCheckingOut} value={rugPullDate} onChange={(e) => setRugPullDate(e.target.value)} type="date" />
+									</Form.Group>
+								</Col>
+								<Col className="col-md-4">
+									<Form.Group controlId="rugPullTime">
+										<Form.Control disabled={isCheckingOut} value={rugPullTime} onChange={(e) => setRugPullTime(e.target.value)} type="time" />
+									</Form.Group>
+								</Col>
+							</>
+						: null }
+					</Row>
+					</>
+					: null
+				}
 
 				{
 					!isCheckingOut ? 
-					<Button variant="primary" onClick={previewCounterfeit}>
+					<Button variant="primary" onClick={previewCounterfeit} disabled={!checkoutReady}>
 						Go to Checkout
 					</Button>
 					: 				
@@ -153,6 +223,12 @@ function Counterfeit(props) {
 							</h3>
 							<NFTMeta meta={order.NFTMeta}></NFTMeta>
 						</Col>
+						{ includeRugPull ? <Col className="col-md-6">
+							<h3>
+								Rug Pull Details
+							</h3>
+							<NFTMeta meta={order.NFTMeta} editable={true} changes={(v) => setRugPullMeta(v)}></NFTMeta>
+						</Col> : null }
 					</Row>
 
 
--- src/components/ui/nftmeta.js
+++ src/components/ui/nftmeta.js
@@ -1,50 +1,135 @@
 import React, { Component, useEffect, useState } from 'react';
 import { Row, Col, Form, Button } from 'react-bootstrap';
+import { Formik, FieldArray } from 'formik';
+import moment from 'moment';
+import { Trash } from 'react-bootstrap-icons';
 
 function NFTMeta(props) {
+    let isModel = (u) => {
+        let fileExt = u.split('.').pop();
+        if (!fileExt) {
+            return false;
+        }
+
+        return fileExt.toLowerCase() === "glb";
+    };
+
+    let isVideo = (u) => {
+        let fileExt = u.split('.').pop();
+        if (!fileExt) {
+            return false;
+        }
+
+        return fileExt.toLowerCase() === "mp4" || fileExt.toLowerCase() === "ogg" || fileExt.toLowerCase() === "webm" || fileExt.toLowerCase() === "ogv";
+    };
+
+    let displayElement = (handleChange, animationURL, imageURL) => {
+        if (animationURL) {
+            if (isModel(animationURL)) {
+                return <model-viewer max-width="250px" auto-rotate="true" autoplay="true" camera-controls="true" src={animationURL} ar-status="not-presenting"></model-viewer>
+            } 
+            if (isVideo(animationURL)) {
+                return <video height="200px" max-width="250px" muted={true} autoPlay={true} controlsList="nodownload" loop={true} preload="auto" src={animationURL}></video>
+            } 
+        }
+
+        return <img height="200px" max-width="250px" src={imageURL} alt="Error loading contract pic"></img>
+    };
 
 	return (
-		<div>
-            <Row>
-                <Col className="col-md-6">
-                    <img height="200px" src={props.meta.image} alt="Error loading contract pic"></img>
-                </Col>
-                <Col className="col-md-6">
-                    <Form.Group controlId="nftmeta">
-                        <Form.Label>Name</Form.Label>
-                        <Form.Control disabled={true} type="text" placeholder="NFT Name, go nuts" value={props.meta.name} />
-                    </Form.Group>
-                    <Form.Group controlId="nftmeta">
-                        <Form.Label>Description</Form.Label>
-                        <Form.Control as="textarea" disabled={true}  style={{height:"100%"}} placeholder="NFT Description, make sure you sell its uniquness hard" value={props.meta.description} />
-                    </Form.Group>
-                </Col>
-            </Row>
-            <Row>
-                <Col className="col-md-12">
-                    <Form.Group controlId="nftmeta">
-                        <Form.Label>Image URL</Form.Label>
-                        <Form.Control disabled={true} type="text" placeholder="Image URL" value={props.meta.image} />
-                    </Form.Group>
-                </Col>
-            </Row>
-            <h4>Attributes</h4>
-            <Row>
-                {
-                    props.meta.attributes.map((val, idx) => { 
-                        return <Col key={`attr_${idx}`} className="col-md-4">
-                            <Form.Group controlId="nftmeta">
-                                <Form.Label>{`${val.trait_type}`}</Form.Label>
-                                <Form.Control disabled={true} type="text" placeholder="Image URL" value={val.value} />
-                                {val.display_type ? <Form.Text className="text-muted">
-                                    {val.display_type}
-                                </Form.Text> : null}
-                            </Form.Group>
-                        </Col>
-                    })
-                }
-            </Row>
-		</div>
+		<Formik
+            initialValues={props.meta}
+            validate={values => {
+                const errors = {};
+                return errors;
+            }}
+        >
+        {({
+         values,
+         handleChange
+       }) => (
+           <>
+             {props.changes && props.changes(values)}
+                <Row>
+                    <Col className="col-md-6">
+                        {displayElement(handleChange, values.animation_url, values.image)}
+                    </Col>
+                    <Col className="col-md-6">
+                        <Form.Group controlId={"nftmeta-name"+values.NFTID}>
+                            <Form.Label>Name</Form.Label>
+                            <Form.Control onChange={handleChange} name="name" disabled={!props.editable} type="text" placeholder="NFT Name, go nuts" value={values.name} />
+                        </Form.Group>
+                        <Form.Group controlId={"nftmeta-desc"+values.NFTID}>
+                            <Form.Label>Description</Form.Label>
+                            <Form.Control as="textarea" disabled={!props.editable} onChange={handleChange} name="description" style={{height:"100%"}} placeholder="NFT Description, make sure you sell its uniquness hard" value={values.description} />
+                        </Form.Group>
+                    </Col>
+                </Row>
+                <h4>Attributes</h4>
+                <Row>
+                    <FieldArray name="attributes">
+                    {({ insert, remove, push }) => (
+                        values.attributes.map((val, idx) => { 
+                            val.formattedVal = val.value;
+
+                            if (val.value === "number") {
+                                val.inputType = "number";
+                            } else {
+                                val.inputType = "text";
+                            }
+
+                            if (val.max_value) {
+                                val.formattedVal = `${val.value} of ${val.max_value}`;
+                            }
+                            
+                            if (val.display_type === "date") {
+                                val.inputType = "date";
+                                val.formattedVal = moment.unix(val.value).format("YYYY-MM-DD");
+                            }
+
+                            let disabled = !props.editable || val.origin === "otc" || !!val.max_value;
+
+                            return <Col key={idx} className="col-md-4">
+                                {
+                                    <Form.Group controlId={"nftmeta"+idx}>
+                                        <Form.Label>{`${val.trait_type}`}</Form.Label> {!disabled ? <Trash onClick={()=> remove(idx)}></Trash> : null}
+                                        <Form.Control onChange={handleChange} name={`attributes.${idx}.value`} disabled={disabled} type={val.inputType} value={val.formattedVal} />
+                                        {val.display_type ? <Form.Text className="text-muted">
+                                            {val.display_type}
+                                        </Form.Text> : null}
+                                    </Form.Group>
+                                }
+                            </Col>
+                        })
+                    )}
+                    </FieldArray>
+                </Row>
+                <Row>
+                    <Col className="col-md-12">
+                        <Form.Group controlId={"nftmeta-img-url"+values.NFTID}>
+                            <Form.Label>Image URL</Form.Label>
+                            <Form.Control onChange={handleChange} name="image" disabled={!props.editable} type="text" placeholder="Image URL" value={values.image} />
+                        </Form.Group>
+
+                        <Form.Group controlId={"nftmeta-ext-url"+values.NFTID}>
+                            <Form.Label>External URL</Form.Label>
+                            <Form.Control onChange={handleChange} name="external_url"  disabled={!props.editable} type="text" value={values.external_url} />
+                        </Form.Group>
+
+                        <Form.Group controlId={"nftmeta-youtube-url"+values.NFTID}>
+                            <Form.Label>Youtube URL</Form.Label>
+                            <Form.Control onChange={handleChange} name="youtube_url"  disabled={!props.editable} type="text" value={values.youtube_url} />
+                        </Form.Group>
+
+                        <Form.Group controlId={"nftmeta-animation-url"+values.NFTID}>
+                            <Form.Label>Animation URL</Form.Label>
+                            <Form.Control onChange={handleChange} name="animation_url" disabled={!props.editable} type="text" value={values.animation_url} />
+                        </Form.Group>
+                    </Col>
+                </Row>
+                </>
+            )}
+		</Formik>
 	);
 }
 
