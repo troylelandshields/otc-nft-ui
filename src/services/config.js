e4583f4f1c87ad2c2d74bc138939d28db4953385
Troy Shields <troy.leland.shields@gmail.com>
2021-04-01 21:58:35 -0600

    dont need this

--- src/services/config.js
+++ src/services/config.js
@@ -2,7 +2,6 @@ const config = {
     // apiHost: "http://localhost:8000",
     apiHost: "https://otc-nft-api.herokuapp.com",
     env: "rinkeby",
-    ethAPIProvider: "https://eth-rinkeby.alchemyapi.io/v2/sl7kpn-EhhbJ_6B39V4uuMQNzGRjl-ei",
     etherscanAPIKey: null,
     contractAddr: "0xDC876c23Cf2f6AB6000a54e0587Dffa0A2c17151",
     nftAddr: "0x00149C791aA70e2970eA2d90218fB66B46888a5D",
